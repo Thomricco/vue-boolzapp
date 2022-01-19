6307bd23168860f80263e6f305ec732a547c20ee
Thomricco <tommasoricco@hotmail.it>
2022-01-19 13:23:31 +0100

    m2 (pausa)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 new Vue({
     el: "#app",
     data: {
-        
+        currentIndex: 0,
         contacts: [
             {
                 name: 'Michele',
@@ -47,13 +47,64 @@ new Vue({
                     }
                 ],
             },
-        
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
         ],
+        textTemoporaneo: '',
+        
     },
-    
+
+    methods: {
+        inviaMessaggio: function() {
+            this.todoList.push({text: this.textTemoporaneo, text: ''})
+            this.textTemoporaneo = ''
+        },
+
+        
+    }
    /* methods:{
         ottieniImmagine (numero){
             return 'img/avatar' + numero + '.jpg';
         }
     }*/
 })
+
+
+
